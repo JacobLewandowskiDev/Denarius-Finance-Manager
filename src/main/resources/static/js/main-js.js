b7465547db011d00d704bb6bff7243a8d9fbc775
Jacob <lewandowskiJakub9@gmail.com>
2022-03-03 22:23:27 -0500

    Initial comit

---
+++ src/main/resources/static/js/main-js.js
@@ -0,0 +1,53 @@
+
+// Elements to observe 
+const textDiv = document.querySelector('.why-denarius');
+const fromLeftDiv = document.querySelectorAll('.information-1');
+const fromRightDiv = document.querySelectorAll('.information-2');
+const mainImage = document.querySelector('.info-main-image');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+    
+            if (entry.isIntersecting) {
+                textDiv.classList.add('fade-in');
+                fromLeftDiv[0].classList.add('from-left');
+                fromRightDiv[0].classList.add('from-right');
+                fromLeftDiv[1].classList.add('from-left');
+                fromRightDiv[1].classList.add('from-right');
+                mainImage.classList.add('from-right');
+                return; 
+              }
+        }); 
+    }, {threshold: .5});
+    
+    observer.observe(document.querySelector('.info-section'));
+
+// Open mobile menu
+var mobileMenuOpened = false;
+document.getElementById('mobile-menu').onclick=function() {
+    if(mobileMenuOpened == false) {
+        document.getElementById('nav-links').style.animation='.5s openMobile forwards ease-in-out';
+        document.getElementById('mobile-close').style.animation='.5s openMobile forwards ease-in-out';
+        mobileMenuOpened = true;
+        console.log('Mobile menu opened');
+    }
+    else if(mobileMenuOpened == true) {
+        document.getElementById('nav-links').style.animation='.5s closeMobile forwards ease-in-out';
+        document.getElementById('mobile-close').style.animation='.5s closeMobile forwards ease-in-out';
+        mobileMenuOpened = false;
+        console.log('Mobile menu closed');
+    }
+}
+
+// Close mobile menu
+var mobileMenuOpened = false;
+document.getElementById('mobile-close').onclick=function() {
+    if(mobileMenuOpened == true) {
+        document.getElementById('nav-links').style.animation='.5s closeMobile forwards ease-in-out';
+        document.getElementById('mobile-close').style.animation='.5s closeMobile forwards ease-in-out';
+        mobileMenuOpened = false;
+        console.log('Mobile menu closed');
+    }
+}
+
+
