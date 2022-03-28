03f4f95ccf54f7ba30d2a813ea9a12eb1e082cf6
Jacob <lewandowskiJakub9@gmail.com>
2022-03-27 20:54:42 -0400

    Database connectivity and authentication

--- src/main/java/com/financeapp/config/SecurityConfig.java
+++ src/main/java/com/financeapp/config/SecurityConfig.java
@@ -1,40 +1,39 @@
 package com.financeapp.config;
 
+import com.financeapp.service.DetailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import javax.annotation.PostConstruct;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private DetailService detailService;
+
     // Password encoder variable
     private final PasswordEncoder passwordEncoder;
 
     // Instantiate the password encoder via constructor
     @Autowired
-    public SecurityConfig(PasswordEncoder passwordEncoder) {
+    public SecurityConfig(PasswordEncoder passwordEncoder, DetailService detailService) {
         this.passwordEncoder = passwordEncoder;
+        this.detailService = detailService;
     }
 
     // Spring security configuration for the web-app
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-//            .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-//            .and()
                 .csrf().disable()
-            .authorizeRequests()
+                .authorizeRequests()
                 .antMatchers("/", "/index", "/css/**", "/js/**", "/images/**").permitAll() // Allow all users to freely access <index.html> page
             .and()
                 .authorizeRequests()
@@ -44,7 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .formLogin()    // Allow form login for users
                 .loginPage("/login")    // Change default login page to custom login popup on <index.html>
                 .permitAll()
-                .defaultSuccessUrl("/userinterface.html", true)
+                .defaultSuccessUrl("/userinterface.html", true) // Set <userinterface.html> as the default success url after a correct login attempt
                 .usernameParameter("username")
                 .passwordParameter("password")
             .and()
@@ -62,22 +61,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-        UserDetails normalUser = User.builder()
-                .username("user")
-                .password(passwordEncoder.encode("pass"))
-                .roles("USER")
-                .build();
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(detailService);
+    }
 
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder.encode("pass"))
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(
-                normalUser,
-                admin
-        );
+    @PostConstruct
+    public void getPassword() {
+        System.out.println(passwordEncoder.encode("pass"));
     }
 }
--- src/main/java/com/financeapp/controller/StaticController.java
+++ src/main/java/com/financeapp/controller/StaticController.java
@@ -1,5 +1,6 @@
 package com.financeapp.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -9,8 +10,9 @@ public class StaticController {
 
     // Redirect back to the login page <index.html> whenever a "/login" request is sent
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public ModelAndView method() {
+    public ModelAndView redirectUser() {
         System.out.println("log in attempt");
+        System.out.println(SecurityContextHolder.getContext());
         return new ModelAndView("index.html");
     }
 }
